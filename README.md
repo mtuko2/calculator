1a33e9b25cab67f916836529c33dc0024f6e3c1d
mtuko2 <mtuko2@gmail.com>
2024-02-04 11:50:30 +0300

    Add files via upload
    meal cost

---
+++ README.md
@@ -0,0 +1,1 @@
+meal cost calculator project
---
+++ meal_cost.py
@@ -0,0 +1,11 @@
+meal_cost = float(input( "Enter the cost of the meal"))
+sales_tax =0.07*meal_cost
+tip = 0.18*meal_cost
+
+
+tip_amount =tip/100*meal_cost
+total_meal_cost =meal_cost+sales_tax+tip_amount
+print("The cost of your meal is: Ksh{:.2f}".format (meal_cost)) 
+print("Total taxes is: Ksh {:.2f}".format (sales_tax)) 
+print("The tip is: Ksh {:.2f}" .format (tip_amount))
+print("The total cost of your meal is: Ksh {:.2f}".format (total_meal_cost)) 
